4c14402ec680f4ddab61432412a9e7963c925be7
ForeverPlayable <gabrodpul@gmail.com>
2024-05-03 16:52:58 +0000

    Added Layers.cs

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Linq;
+
+/// <summary>
+/// Static class containing const ints for Unity layers.
+/// Simply copy & paste.
+/// Meant to make switching on layers easier and all of that.
+/// </summary>
+public static class Layers
+{
+    public const int Default        = 0;
+    public const int TransparentFX  = 1;
+    public const int IgnoreRaycast  = 2;
+    // public const int Layer3         = 3;
+    public const int Water          = 4;
+    public const int UI             = 5;
+
+    public static readonly int[] Array
+        = typeof( Layers )
+            .GetFields( BindingFlags.Public | BindingFlags.Static )
+            .Where( (fi) => fi.IsLiteral
+                         && !fi.IsInitOnly
+                         && fi.FieldType == typeof(int) )
+            .Select( (v) => (int)v.GetRawConstantValue() )
+            .ToArray();
+}
